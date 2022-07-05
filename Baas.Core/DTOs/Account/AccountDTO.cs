d8a61ae5452d00a0e9ae1e160fde80a05e86e2cf
rober-gs <roberto.garcias@hotmail.com>
2022-07-04 20:42:26 -0500

    add account controller and functionality

--- Baas.Core/DTOs/Account/AccountDTO.cs
+++ Baas.Core/DTOs/Account/AccountDTO.cs
@@ -6,11 +6,11 @@ namespace Baas.Core.DTOs
     {
         [JsonPropertyName("address")]
         public string Address { get; set; }
-        [JsonPropertyName("privateKey")]
-        public string PrivateKey { get; set; }
-        [JsonPropertyName("password")]
-        public string Password { get; set; }
-        [JsonPropertyName("fileJson")] 
-        public string JsonFile { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("balance")]
+        public decimal Balance { get; set; }
     }
 }
